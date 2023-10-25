47e4e92e93d250a3e4c74829b2e953f721e3a1d1
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-10-25 19:38:55 +0300

    Lights, background color

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+//Set up model loader and view renderer
 const loader = new GLTFLoader();
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0xeeeeee, 1);
+document.body.appendChild(renderer.domElement);
 
 //Set up scene and camera
 const scene = new THREE.Scene();
@@ -13,9 +20,11 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+//Add lights
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+directionalLight.position.set(0, 10, 10);
+scene.add(directionalLight, ambientLight);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
