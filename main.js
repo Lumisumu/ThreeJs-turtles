ebb52fdac6482d5c41429d5e5e1ffc3c3803f5b8
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2023-10-25 19:59:11 +0300

    Create material, orbit controls to camera

--- main.js
+++ main.js
@@ -2,23 +2,33 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-//Set up model loader and view renderer
+//3D model loader
 const loader = new GLTFLoader();
+
+//Create scene
+const scene = new THREE.Scene();
+
+//Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setClearColor(0xeeeeee, 1);
+renderer.setClearColor(0x000000, 1);
 document.body.appendChild(renderer.domElement);
 
-//Set up scene and camera
-const scene = new THREE.Scene();
+//Camera (FOV, aspect ratio, clipping dist near, clipping dist far)
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.z = 5;
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(0, 1, 5);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.maxDistance = 10;
+controls.minDistance = 1;
+controls.maxPolarAngle = Math.PI / 2;
 
 //Add lights
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -26,14 +36,27 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 directionalLight.position.set(0, 10, 10);
 scene.add(directionalLight, ambientLight);
 
+//Glass material
+const bowlMaterial = new THREE.MeshPhysicalMaterial({
+  metalness: 0,
+  roughness: 1,
+  envMapIntensity: 0.9,
+  clearcoat: 1,
+  transparent: true,
+  transmission: 0.95,
+  opacity: 1,
+  reflectivity: 0.2,
+});
+
+//Add cube and apply material
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(geometry, bowlMaterial);
 scene.add(cube);
 
 //Render scene
 function animate() {
   requestAnimationFrame(animate);
+  controls.update();
   renderer.render(scene, camera);
 }
 animate();
